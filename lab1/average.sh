4ff0ed4084685591294a0e8198d816d5ec42a317
aynurex <badretdinov12@gmail.com>
2020-03-04 14:05:49 +0000

    added additional part

--- lab1/average.sh
+++ lab1/average.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-for (a=0;a<150;a++)
+cat /dev/null > numbers.txt
+count=$(shuf -i 0-1000 -n 1)
+shuf -i 0-1000 -n $count -o numbers.txt
+sum=0
+for lines in $(cat numbers.txt)
 do
-
+((sum+= lines))
+done
+average=$(( $sum / $count))
+echo "count: $count"
+echo "average: $average" 
