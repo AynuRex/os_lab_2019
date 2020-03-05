ca44a99be469513909e98f89ece43d57df28599e
aynurex <badretdinov12@gmail.com>
2020-03-05 11:07:40 +0000

    Done 1-2 ex

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,19 @@
 #include "revert_string.h"
+#include "../swap/swap.h"
 
 void RevertString(char *str)
 {
+    int len=0;
+    while(str[len]!='\0')
+    {
+        len++;
+    }    
+    for(int i=0;i<len/2;i++)
+    {        
+        Swap( str+i ,str+len -i- 1);
+    }
+
 	// your code here
+
 }
 
